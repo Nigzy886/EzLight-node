437ee5561021d5808578c64152de044c72c9909e
EZsystems <140813324+Nigzy886@users.noreply.github.com>
2026-07-10 13:01:13 +1200

    Add managed node context to EzLight state

--- firmware/EzLight_Node/TelemetryReporter.h
+++ firmware/EzLight_Node/TelemetryReporter.h
@@ -8,5 +8,5 @@
 
 class TelemetryReporter {
 public:
-  String stateJson(const RelayDriver& relays, const ScheduleEngine& schedule, const AstroEngine& astro, const TimeService& timeService) const;
+  String stateJson(const String& nodeId, bool provisioned, const String& ipAddress, const String& hostName, int32_t wifiRssi, const RelayDriver& relays, const ScheduleEngine& schedule, const AstroEngine& astro, const TimeService& timeService) const;
 };
