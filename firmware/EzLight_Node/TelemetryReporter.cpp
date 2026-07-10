49c3da149a8e91c60434935f5e1124340b9638a8
EZsystems <140813324+Nigzy886@users.noreply.github.com>
2026-07-10 13:01:25 +1200

    Report authoritative managed EzLight state

--- firmware/EzLight_Node/TelemetryReporter.cpp
+++ firmware/EzLight_Node/TelemetryReporter.cpp
@@ -1,28 +1,53 @@
 #include "TelemetryReporter.h"
-#include "EzLightTypes.h"
 
-String TelemetryReporter::stateJson(const RelayDriver& relays, const ScheduleEngine& schedule, const AstroEngine& astro, const TimeService& timeService) const {
-  String json = "{\"eco\":\"easysystems\",\"type\":\"state\",\"node_id\":\"" + String(EZLIGHT_NODE_ID) + "\",";
+namespace {
+String jsonEscape(String value) {
+  value.replace("\\", "\\\\");
+  value.replace("\"", "\\\"");
+  value.replace("\n", "\\n");
+  value.replace("\r", "\\r");
+  return value;
+}
+
+String quote(const String& value) {
+  return "\"" + jsonEscape(value) + "\"";
+}
+}  // namespace
+
+String TelemetryReporter::stateJson(const String& nodeId, bool provisioned, const String& ipAddress, const String& hostName, int32_t wifiRssi, const RelayDriver& relays, const ScheduleEngine& schedule, const AstroEngine& astro, const TimeService& timeService) const {
+  String json = "{\"eco\":\"easysystems\",\"type\":\"state\",\"node_id\":" + quote(nodeId) + ",";
   json += "\"relay_state\":{";
   for (uint8_t i = 0; i < EZLIGHT_RELAY_COUNT; ++i) {
-    if (i > 0) { json += ","; }
-    json += "\"" + String(relays.definition(i).id) + "\":" + (relays.logicalState(i) ? "true" : "false");
+    if (i > 0) {
+      json += ",";
+    }
+    json += quote(relays.definition(i).id) + ":" + (relays.logicalState(i) ? "true" : "false");
   }
   json += "},\"schedule_state\":{";
   for (uint8_t i = 0; i < EZLIGHT_RELAY_COUNT; ++i) {
-    if (i > 0) { json += ","; }
-    json += "\"" + String(relays.definition(i).id) + "\":\"" + schedule.stateFor(i) + "\"";
+    if (i > 0) {
+      json += ",";
+    }
+    json += quote(relays.definition(i).id) + ":" + quote(schedule.stateFor(i));
   }
   json += "},";
   json += "\"time_valid\":" + String(timeService.valid() ? "true" : "false") + ",";
-  json += "\"next_event\":\"" + schedule.nextEvent() + "\",";
-  json += "\"dusk_time\":\"" + astro.duskTime() + "\",";
-  json += "\"dawn_time\":\"" + astro.dawnTime() + "\",";
-  json += "\"civil_dusk\":\"" + astro.duskTime() + "\",";
-  json += "\"civil_dawn\":\"" + astro.dawnTime() + "\",";
-  json += "\"uptime\":" + String(millis() / 1000UL);
-  if (timeService.status().warning.length() > 0) { json += ",\"time_warning\":\"" + timeService.status().warning + "\""; }
-  if (astro.warning().length() > 0) { json += ",\"astro_warning\":\"" + astro.warning() + "\""; }
+  json += "\"next_event\":" + quote(schedule.nextEvent()) + ",";
+  json += "\"dusk_time\":" + quote(astro.duskTime()) + ",";
+  json += "\"dawn_time\":" + quote(astro.dawnTime()) + ",";
+  json += "\"civil_dusk\":" + quote(astro.duskTime()) + ",";
+  json += "\"civil_dawn\":" + quote(astro.dawnTime()) + ",";
+  json += "\"uptime\":" + String(millis() / 1000UL) + ",";
+  json += "\"provisioned\":" + String(provisioned ? "true" : "false") + ",";
+  json += "\"ip\":" + quote(ipAddress) + ",";
+  json += "\"host\":" + quote(hostName) + ",";
+  json += "\"wifi_rssi\":" + String(wifiRssi);
+  if (timeService.status().warning.length() > 0) {
+    json += ",\"time_warning\":" + quote(timeService.status().warning);
+  }
+  if (astro.warning().length() > 0) {
+    json += ",\"astro_warning\":" + quote(astro.warning());
+  }
   json += "}";
   return json;
 }
