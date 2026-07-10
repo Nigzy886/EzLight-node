c070bc966200f89ff7cfa7fc9da8e37b800bfaff
EZsystems <140813324+Nigzy886@users.noreply.github.com>
2026-07-10 13:00:38 +1200

    Make EzLight identity and provisioning state authoritative

--- firmware/EzLight_Node/EzHubCapabilities.h
+++ firmware/EzLight_Node/EzHubCapabilities.h
@@ -4,6 +4,6 @@
 
 class EzHubCapabilities {
 public:
-  String helloJson(const String& ipAddress, const String& hostName) const;
-  String capabilitiesJson() const;
+  String helloJson(const String& nodeId, const String& ipAddress, const String& hostName, const String& friendlyName, bool provisioned) const;
+  String capabilitiesJson(const String& nodeId, const String& ipAddress, const String& hostName, bool provisioned) const;
 };
